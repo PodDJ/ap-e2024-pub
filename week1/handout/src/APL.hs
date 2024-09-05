991ecbac1f183e08143a9dbb0cfe176d46f5c660
PodDJ <danieljaksland@gmail.com>
2024-09-05 19:40:09 +0200

    stufflol

--- week1/handout/src/APL.hs
+++ week1/handout/src/APL.hs
@@ -1,4 +1,113 @@
 module APL
   (
+    Exp(..),
+    Val(..),
+    eval,
+    eval
   )
 where
+
+data Exp
+  = CstInt Integer
+  | Add Exp Exp
+  | Sub Exp Exp
+  | Mul Exp Exp
+  | Div Exp Exp
+  | Pow Exp Exp
+  | CstBool Bool
+  | Eql Exp Exp
+  | If Exp Exp Exp
+
+  deriving (Eq, Show)
+
+data Val
+  = ValInt Integer
+  | ValBool Bool
+  deriving (Eq, Show)
+
+type Error = String
+{-
+eval :: Exp -> Val
+eval (CstInt x) = (ValInt x)
+eval (Add x y) = ValInt(xVal + yVal)
+  where 
+    ValInt xVal = eval x
+    ValInt yVal = eval y
+
+eval (Sub x y) = ValInt(xVal - yVal)
+  where
+      ValInt xVal = eval x
+      ValInt yVal = eval y
+
+eval (Mul x y) = ValInt (xVal * yVal)
+  where
+    ValInt xVal = eval x
+    ValInt yVal = eval y
+
+eval (Div x y) = 
+  if yVal == 0 then error "cant divide by 0"
+  else ValInt (xVal `div` yVal)
+  where
+    ValInt xVal = eval x
+    ValInt yVal = eval y
+
+eval (Pow x y) = 
+  if yVal < 0 then error "negative exponents not supported for ints"
+  else ValInt (xVal ^ yVal)
+  where
+    ValInt xVal = eval x
+    ValInt yVal = eval y
+-}
+
+eval :: Exp -> Either Error Val
+eval (CstInt x) = Right $ ValInt x
+
+eval (Add x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt xVal), Right (ValInt yVal)) -> Right $ ValInt $ xVal + yVal
+
+eval (Sub x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt xVal), Right (ValInt yVal)) -> Right $ ValInt $ xVal - yVal
+
+eval (Mul x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt xVal), Right (ValInt yVal)) -> Right $ ValInt $ xVal * yVal
+
+eval (Div x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (_, Right (ValInt 0)) -> Left $ "Division by 0"
+    (Right (ValInt xVal), Right (ValInt yVal)) -> Right $ ValInt $ xVal `div` yVal
+
+eval (Pow x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt xVal), Right (ValInt yVal)) -> 
+      if yVal < 0 then Left "Negative exponents not allowed"
+      else Right $ ValInt $ xVal ^ yVal
+
+eval (CstBool x) = Right $ ValBool x
+
+eval (Eql x y) = 
+  case (eval x, eval y) of
+    (Left err, _) -> Left "Cant compare with error"
+    (_, Left err) -> Left "Cant compare with error"
+    (Right (ValBool v), Right (ValInt r)) -> Left "Comparison of different types not allowed"
+    (Right (ValInt v), Right (ValBool r)) -> Left "Comparison of different types not allowed"
+    (e1, e2) -> if e1 == e2 then Right $ ValBool $ True else Right $ ValBool $ False
+    
+eval (If cond e1 e2) =
+  case eval cond of
+    Left err -> Left err
+    Right (ValBool True) -> eval e1
+    Right (ValBool False) -> eval e2
+    Right _ -> Left "not a bool"
--- week1/handout/src/APL_Tests.hs
+++ week1/handout/src/APL_Tests.hs
@@ -3,9 +3,139 @@ module APL_Tests (tests) where
 import APL ()
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit ()
+import APL (Exp (..), Val (..), eval)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [
+{-      testCase "Convert" $
+        eval (CstInt 5)
+        @?=  
+        (ValInt 5),
+
+      testCase "Add" $
+        eval (Add (CstInt 5) (CstInt 8)) 
+        @?= 
+        ValInt 13,
+
+      testCase "Sub" $
+        eval (Sub (CstInt 5) (CstInt 8)) 
+        @?= 
+        ValInt (-3),
+
+      testCase "Mul" $
+        eval (Mul (CstInt 5) (CstInt 8)) 
+        @?=
+        ValInt 40,
+
+      testCase "Div" $
+        eval (Div (CstInt 8) (CstInt 8))
+        @?=
+        ValInt 1,
+
+      {-testCase "DivZero" $ 
+        eval (Div (CstInt 5) (CstInt 0)) 
+        @?=
+        ValInt 2,
+        --always fails--
+        -}
+
+      testCase "Pow" $
+        eval (Pow (CstInt 2) (CstInt 5)) 
+        @?= 
+        ValInt 32,
+-}
+      testCase "Combo" $
+        eval (Add (Sub (CstInt 9) (CstInt 5)) (Mul (Pow (CstInt 2) (CstInt 3)) (Div (CstInt 8) (CstInt 4))) )
+        @?= 
+        Right(ValInt 20),
+
+
+      testCase "Convert" $
+        eval (CstInt 5) 
+        @?= 
+        Right (ValInt 5),
+
+      testCase "Add" $
+        eval (Add (CstInt 5) (CstInt 8)) 
+        @?= 
+        Right(ValInt 13),
+
+      testCase "Div0" $
+        eval (Div (CstInt 5) (CstInt 0)) 
+        @?= 
+        Left "Division by 0",
+
+      testCase "Div02" $
+        eval (Div (CstInt 2) (Sub (CstInt 2) (CstInt 2) )) 
+        @?= 
+        Left "Division by 0",
+
+      testCase "Div" $
+        eval (Div (CstInt 5) (CstInt 2)) 
+        @?= 
+        Right(ValInt 2),
+  
+      testCase "PowNegativeExponent" $
+        eval (Pow (CstInt 5) (CstInt (-4))) 
+        @?= 
+        Left "Negative exponents not allowed",
+
+      testCase "PowNegativeBase" $
+        eval (Pow (CstInt (-4)) (CstInt 2)) 
+        @?= 
+        Right(ValInt 16),
+
+      testCase "Pow" $
+        eval (Pow (CstInt 5) (CstInt 2)) 
+        @?= 
+        Right(ValInt 25),
+  
+      testCase "ConvertBool" $
+        eval (CstBool True)
+        @?= 
+        Right(ValBool True),
+
+      testCase "EqlTrueTrue" $
+        eval (Eql (CstBool True) (CstBool True))
+        @?= 
+        Right(ValBool True),
+
+      testCase "EqlFalseTrue" $
+        eval (Eql (CstBool True) (CstBool False))
+        @?= 
+        Right(ValBool False),
+
+      testCase "Eql5_4" $
+        eval (Eql (CstInt 5) (CstInt 4))
+        @?= 
+        Right(ValBool False),
+
+      testCase "Eql5_5" $
+        eval (Eql (CstInt 5) (CstInt 5))
+        @?= 
+        Right(ValBool True),
+
+      testCase "EqlErrs" $
+        eval (Eql (Div (CstInt 5) (CstInt 0)) (Pow (CstInt 5) (CstInt (-2))))
+        @?= 
+        Left "Cant compare with error",
+
+      testCase "EqlDifferentTypes" $
+        eval (Eql (CstInt 1) (CstBool True))
+        @?= 
+        Left "Comparison of different types not allowed",
+
+      testCase "If1" $
+        eval (If (CstBool True) (CstInt 4) (CstInt 3))
+        @?= 
+        Right (ValInt 4),
+
+      testCase "If2" $
+        eval (If (CstBool False) (CstInt 4) (CstInt 3))
+        @?= 
+        Right (ValInt 3)
+    ]
